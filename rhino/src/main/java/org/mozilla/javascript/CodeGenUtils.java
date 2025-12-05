86b3ece457989d49616f1c7530c1557f34439f76
Ronald Brill <rbri@rbri.de>
2025-12-05 10:22:47 +0100

    HtmlUnit/JDK

--- rhino/src/main/java/org/mozilla/javascript/CodeGenUtils.java
+++ rhino/src/main/java/org/mozilla/javascript/CodeGenUtils.java
@@ -148,10 +148,10 @@ public class CodeGenUtils {
      */
     private static String[] disambiguateNames(String[] names, int paramCount) {
         String[] result = new String[names.length];
-        var checkMap = new HashMap<String, AtomicInteger>();
+        HashMap<String, AtomicInteger> checkMap = new HashMap<String, AtomicInteger>();
         for (int i = 0; i < names.length; i++) {
-            var counter = checkMap.computeIfAbsent(names[i], k -> new AtomicInteger());
-            var count = counter.getAndIncrement();
+            AtomicInteger counter = checkMap.computeIfAbsent(names[i], k -> new AtomicInteger());
+            int count = counter.getAndIncrement();
             if (i >= paramCount && count > 0) {
                 result[i] = String.format("%s(%d)", names[i], count);
             } else {
